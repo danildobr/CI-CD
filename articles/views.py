7c1899c220ae4f47da1c3af14a584c7619f21ba0
Danil_Dobr <danil.bore@mail.ru>
2025-08-16 14:23:21 +0500

    лучше предыдущей+12

--- articles/views.py
+++ articles/views.py
@@ -12,6 +12,6 @@ def articles_list(request):
 
 
 def cicd(request):
-    current_time = 'сегодня, новая версия, лучше предыдущей'
+    current_time = 'сегодня, новая версия, лучше предыдущей, может надо было подождать? '
     msg = f'Текущее время: {current_time}'
     return HttpResponse(msg)
